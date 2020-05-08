2d82b4a264e47fe25cf8dc8b1c733a6d91e86d41
gafeol <gabfeol@gmail.com>
2020-05-08 10:36:57 -0300

    Reset game to whack + max score

--- whack-a-mole/index.js
+++ whack-a-mole/index.js
@@ -48,6 +48,22 @@ const shuffle = (arr) => {
 const clickCell = () => {
     var score = document.getElementById('score');
     score.textContent = parseInt(score.textContent) + 1;
+    var maxscore = document.getElementById('maxscore');
+    maxscore.textContent = Math.max(parseInt(score.textContent), parseInt(maxscore.textContent));
 };
 
-intervalId = setInterval(resetMoles, 1000, 1);
+intervalId = setInterval(resetMoles, 1000);
+
+
+const resetGame = () => {
+    document.getElementById('score').textContent = 0;
+    document.getElementById('time').textContent = 10;
+    const moleDivs = document.querySelectorAll(".mole");
+    moleDivs.forEach(moleDiv => {
+        moleDiv.classList.remove('mole');
+        moleDiv.removeEventListener('click', clickCell);
+    });
+    clearInterval(intervalId);
+    intervalId = setInterval(resetMoles, 1000);
+
+}
